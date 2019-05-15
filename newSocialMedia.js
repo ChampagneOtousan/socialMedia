78954f6463949209db1c7e589703b2a993b2ab2c
Esteban Cuesta <cuestaesteban@yahoo.com>
2019-05-15 14:51:01 -0400

    able to send and store messages on user2 inbox

--- newSocialMedia.js
+++ newSocialMedia.js
@@ -19,3 +19,19 @@ class Message {
 
 user1 = new User("estebanito@platano.com", "swaggity")
 user2 = new User("estebano@plantain.com", "hulusucks")
+
+let user1form = document.getElementById("user1-form")
+let user2inbox = document.getElementById("user2-inbox")
+
+function sendMessage() {
+  let text = document.getElementById("msg-content")
+  let p = document.createElement("p")
+  p.innerText = text.value
+  user2inbox.appendChild(p)
+  alert(text.value)
+}
+
+user1form.addEventListener("submit", function(event) {
+  event.preventDefault()
+  sendMessage()
+})

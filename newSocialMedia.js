995ac1d860fc74b03551a146994fbe38f80def70
Esteban Cuesta <cuestaesteban@yahoo.com>
2019-05-15 15:58:54 -0400

    finish adding block user function

--- newSocialMedia.js
+++ newSocialMedia.js
@@ -19,16 +19,28 @@ class Message {
 
 user1 = new User("estebanito@platano.com", "swaggity")
 user2 = new User("estebano@plantain.com", "hulusucks")
+user3 = new User("pansamala@korea.com", "huskyhater")
 
 let user1form = document.getElementById("user1-form")
 let user2inbox = document.getElementById("user2-inbox")
+window.addEventListener("keydown", function(event) {
+  if (event.which == 13) {
+    sendMessage()
+  }
+})
+//
+// function blockUser() {
+//   let block = getElementById("user3")
+//
+// }
 
 function sendMessage() {
   let text = document.getElementById("msg-content")
   let p = document.createElement("p")
   p.innerText = text.value
   user2inbox.appendChild(p)
-  alert(text.value)
+  text.value = ""
+  text.focus()
 }
 
 user1form.addEventListener("submit", function(event) {
